39fc5161094742d67524a67bb795cc3dfa48c775
nviliam <nviliam7@gmail.com>
2026-07-14 12:31:27 +0200

    group-match different bakground designs

---
+++ src/app/(app)/groups/[id]/layout.tsx
@@ -0,0 +1,7 @@
+export default function GroupDetailLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="page-bg-group">{children}</div>;
+}
---
+++ src/app/(app)/groups/[id]/matches/[matchId]/layout.tsx
@@ -0,0 +1,7 @@
+export default function MatchDetailLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="page-bg-match">{children}</div>;
+}
